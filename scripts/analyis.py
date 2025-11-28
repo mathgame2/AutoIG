8e6ce2ed7a64fb15e6d2f0ad91eb7eabb9657684
TinTin <tw74@st-andrews.ac.uk>
2025-11-28 11:29:37 +0000

    Uploading changes from experiments

---
+++ scripts/analyis.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import json
+import os
+import sys
+import argparse
+from collections import Counter
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    # general settings
+    parser.add_argument(
+        "--fileName",
+        required=True,
+        type=str,
+        help="path to the analysis file (json)"
+    )
+
+
+
+    # read all settings into one variable and check setting validity
+    args = parser.parse_args()
+    with open(args.fileName, 'r') as f:
+        f.readline()
+        # print(f.readline())
+        lsLines = []
+        for s in f.readlines():
+            data = json.loads(s)
+            if "ok" in data["status"]:
+                print(s)
+            lsLines.append(data["status"])
+    print(Counter(lsLines).items())
+
+
+
+main()
